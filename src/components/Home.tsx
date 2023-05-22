15542933056c38b6ef500ebb1c897b63dfa67a89
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-22 14:54:54 +0100

    creates Home component and styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
 
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section>
+            <div className="welcome">
+                <h1>Bot, Process, Efficiency</h1>
+                <h1>Robotic Process Automation that unifies people, process and technology</h1>
+                <h1>We help business leaders embrace AI-Powered Automation as they endeavor to visualize the future.</h1>
+            </div>
+        </section>
+    )
 }
 
 export default Home
