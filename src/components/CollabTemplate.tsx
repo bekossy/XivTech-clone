5e80a3501799b8a2b22eb3ff0c3670f179f4f032
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-23 03:21:56 +0100

    adds keys to mapped components

--- src/components/CollabTemplate.tsx
+++ src/components/CollabTemplate.tsx
@@ -13,15 +13,15 @@ interface Props {
 const CollabTemplate: React.FC<Props> = ({ bgImg, title, text, info, moreInfo, capArray }) => {
     return (
         <div className="template">
-            <div className="templatebg" style={{ background: `rgba(0, 0, 0, 0.4) url(${bgImg})`, backgroundRepeat: "no-repeat", backgroundSize: "100%", backgroundPosition: "center", backgroundBlendMode: "darken", backgroundAttachment: "fixed" }}>
+            <div className="templatebg" style={{ background: `rgba(0, 0, 0, 0.4) url(${bgImg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "50% center", backgroundBlendMode: "darken", backgroundAttachment: "fixed" }}>
                 <h1>{title}</h1>
                 <p>{text}</p>
             </div>
 
             <div className="templateInfo">
                 <div className="tempInfoTitle">
-                    {info.map((infoText) => (
-                        <p>{infoText}</p>
+                    {info.map((infoText, index) => (
+                        <p key={index}>{infoText}</p>
                     ))}
                 </div>
 
@@ -39,8 +39,8 @@ const CollabTemplate: React.FC<Props> = ({ bgImg, title, text, info, moreInfo, c
                             <img src={card.img} alt="" />
 
                             <div className="cardText">
-                                {card.text.map((cardText) => (
-                                    <p>{cardText}</p>
+                                {card.text.map((cardText, index) => (
+                                    <p key={index}>{cardText}</p>
                                 ))}
                             </div>
                         </div>
