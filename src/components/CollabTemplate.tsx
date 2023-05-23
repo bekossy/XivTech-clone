77a041fdf6305ba7d16f86ef2e503c78cb248821
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-23 01:58:26 +0100

    create dynamic template for route pages

---
+++ src/components/CollabTemplate.tsx
@@ -0,0 +1,55 @@
+interface Props {
+    bgImg: string;
+    title: string;
+    text: string;
+    info: string[];
+    moreInfo: string;
+    capArray: {
+        img: string;
+        text: string[];
+    }[];
+}
+
+const CollabTemplate: React.FC<Props> = ({ bgImg, title, text, info, moreInfo, capArray }) => {
+    return (
+        <div className="template">
+            <div className="templatebg" style={{ background: `rgba(0, 0, 0, 0.4) url(${bgImg})`, backgroundRepeat: "no-repeat", backgroundSize: "100%", backgroundPosition: "center", backgroundBlendMode: "darken", backgroundAttachment: "fixed" }}>
+                <h1>{title}</h1>
+                <p>{text}</p>
+            </div>
+
+            <div className="templateInfo">
+                <div className="tempInfoTitle">
+                    {info.map((infoText) => (
+                        <p>{infoText}</p>
+                    ))}
+                </div>
+
+                <p>{moreInfo}</p>
+
+                <button className="btn">Get in touch</button>
+            </div>
+
+            <div className="capabilities">
+                <h1>Capabilities</h1>
+
+                <div className="cards">
+                    {capArray.map((card, index) => (
+                        <div className="card" key={index}>
+                            <img src={card.img} alt="" />
+
+                            <div className="cardText">
+                                {card.text.map((cardText) => (
+                                    <p>{cardText}</p>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default CollabTemplate
