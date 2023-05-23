624f151052e1c95db909e1b986c6ab0a102669d1
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-23 03:20:51 +0100

    changes auchor tags to Link tags

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,24 +3,25 @@ import Main from "../assets/Logo.svg"
 import search from "../assets/searchIcon.svg"
 import downArrow from '../assets/downArrow.svg'
 import menu from '../assets/menuIcon.svg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <div className="navImg">
+            <Link to={"/"} className="navImg">
                 <img src={Main} alt="" />
                 <img src={Logo} alt="" />
-            </div>
+            </Link>
 
             <div className="navOpt">
                 <div className="navBtns">
-                    <a href="/">
+                    <Link to="/">
                         <p>Services</p>
                         <img src={downArrow} alt="" />
-                    </a>
-                    <a href="/">Careers</a>
-                    <a href="/">About</a>
-                    <a href="/">Contact</a>
+                    </Link>
+                    <Link to="/">Careers</Link>
+                    <Link to="/">About</Link>
+                    <Link to="/">Contact</Link>
                     <img src={search} alt="" />
                 </div>
                 <button type='button' className='navBtn'>Let's Talk</button>
