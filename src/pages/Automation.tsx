53d549a167e6c3d58ccc54d52187de29e1f84ab3
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-23 02:16:29 +0100

    passes Automation down to CollabTemplate

--- src/pages/Automation.tsx
+++ src/pages/Automation.tsx
@@ -1,34 +1,25 @@
-import React from 'react'
+import CollabTemplate from '../components/CollabTemplate';
+import bgImg2 from '../assets/bgImg2.png';
+import capImg1 from '../assets/capabilityImg1.jpg'
 
 const Automation = () => {
+    const capabilitiesArray = [
+        {
+            img: capImg1,
+            text: ["Bot Automation Simplified", "Build, Deploy and Scale Bots"]
+        }
+    ]
     return (
-        <div>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-            <p>Automation</p>
-        </div>
+        <>
+            <CollabTemplate
+                bgImg={bgImg2}
+                title={"AI + RPA = Enabling End-to-End Automation"}
+                text={'Drive Efficiency, Automate and Monetize'}
+                info={["Process", "Automation", "Monetization"]}
+                moreInfo={"Our team of professionals help you deliver measurable business outcomes by strategizing, designing and executing results driven AI + RPA automation. With Ekisu - our flagship AI automation product, we help you with end-to end automation for your workflows. Each automation helps you save time, reduce cost of human capital, and improve efficiency! Now that’s successful Digital Transformation!"}
+                capArray={capabilitiesArray}
+            />
+        </>
     )
 }
 
